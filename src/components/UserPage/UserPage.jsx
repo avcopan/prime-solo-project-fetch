10ed928e489bdc1234a73715dd45045285791a51
Andreas Copan <avcopan@gmail.com>
2023-07-25 20:45:35 -0400

    Gets user-specific information on the front-end

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useState, useEffect } from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector } from "react-redux";
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const [foodItems, setFoodItems] = useState([]);
+
+  const fetchFoodItems = () => {
+    fetch("/api/potluck")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Request not OK.");
+        }
+      })
+      .then((foodItemsFromServer) => {
+        setFoodItems(foodItemsFromServer);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Something went wrong");
+      });
+  };
+
+  useEffect(() => {
+    fetchFoodItems();
+  }, []);
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
+      {foodItems.map(item => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <h4>{item.username}</h4>
+          <h5>{item.type}</h5>
+        </div>
+      ))}
     </div>
   );
 }
